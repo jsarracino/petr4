e90fd4193acebc760b71cd20651a6f3372af84b6
Natalie Isak <ngi2@cornell.edu>
2020-11-01 16:19:38 -0500

    edits based on round trip tests

--- test/test.ml
+++ test/test.ml
@@ -25,6 +25,22 @@ let parser_test include_dirs file =
   | `Ok _ -> true
   | `Error _ -> false
 
+let to_string pp : string =
+  Format.fprintf Format.str_formatter "%a" Pp.to_fmt pp;
+  Format.flush_str_formatter ()
+
+let get_name include_dirs file =
+  match Parse.parse_file include_dirs file false with 
+  | `Ok prog -> prog |> Prettypp.format_program |> to_string 
+  | `Error _ -> "121" 
+
+let pp_round_trip_test include_dirs file =
+  let way_there = match Parse.parse_file include_dirs file false with 
+    | `Ok prog -> prog |> Prettypp.format_program |> to_string 
+    | `Error _ -> "" in 
+  let way_back = Parse.parse_string way_there in
+  String.compare way_there (way_back |> Prettypp.format_program |> to_string) = 0 
+
 let typecheck_test (include_dirs : string list) (p4_file : string) : bool =
   match Parse.parse_file include_dirs p4_file false with
   | `Ok prog ->
@@ -73,7 +89,10 @@ let example_path l =
   let root = Filename.concat ".." "examples" in
   List.fold_left l ~init:root ~f:Filename.concat
 
-let () =
+let () = 
+  Format.printf "%s" (get_name ["../examples"] (example_path ["checker_tests"; "good"; "coree.p4"])); 
+  Format.printf "%s" "----------";
+  Format.printf "%s" ((Parse.parse_string (get_name ["../examples"] (example_path ["checker_tests"; "good"; "coree.p4"]))) |> Prettypp.format_program |> to_string); 
   let open Alcotest in
   run "Tests" [
     (* "parser tests good", (Stdlib.List.map (fun name ->
@@ -81,8 +100,10 @@ let () =
        "typecheck tests good", (Stdlib.List.map (fun name ->
         test_case name `Quick (good_test typecheck_test name)) good_files);
        "typecheck tests bad", (Stdlib.List.map (fun name ->
-        test_case name `Quick (bad_test typecheck_test name)) bad_files); *)
-    "typecheck tests pp", [test_case "printing tests" `Quick pp_test]
+        test_case name `Quick (bad_test typecheck_test name)) bad_files);
+       "unit tests pp", [test_case "printing tests" `Quick pp_test];  *)
+    (* "round trip pp tests good", (Stdlib.List.map (fun name ->
+        test_case name `Quick (good_test pp_round_trip_test name)) good_files); *)
   ]
 
 
